4eb49768eb3f36e8f4420ebe2567c56120cca583
jinkyuhan <gkswlsrb95@gmail.com>
2019-11-26 04:29:22 +0900

    init: Make project structure

---
+++ resourcesharing/src/test/java/com/dbteam6/resourcesharing/ResourcesharingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbteam6.resourcesharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourcesharingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
